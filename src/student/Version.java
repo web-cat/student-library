fa9edc53bbeeead324299321c6df4a72d8eada58
s-edwards <edwards@cs.vt.edu>
2011-02-18 19:18:39 +0000

    v4.04

--- src/student/Version.java
+++ src/student/Version.java
@@ -37,11 +37,11 @@ package student;
  *  </pre>
  *  <p>Either approach will generate this version information:</p>
  *  <pre>
- *  student.Version: package student, v4.02 $Date$
+ *  student.Version: package student, v4.04 $Date$
  *  </pre>
  *
  *  @author Stephen Edwards
- *  @version 4.02
+ *  @version 4.04
  *  @author Last changed by $Author$
  *  @version $Revision$, $Date$
  */
@@ -50,7 +50,7 @@ public class Version
     //~ Instance/static variables .............................................
 
     // These fields are used for overridable world startup
-    private static final String version = "v4.02 $Date$";
+    private static final String version = "v4.04 $Date$";
 
 
     //~ Constructors ..........................................................
