7d37d17e5232d3db9bf6e83b07d49db892faaf5e
s-edwards <edwards@cs.vt.edu>
2011-04-22 17:04:05 +0000

    Fixed errors in normalizing line terminator sequences across different platforms.

--- src/student/testingsupport/PrintStreamWithHistory.java
+++ src/student/testingsupport/PrintStreamWithHistory.java
@@ -45,6 +45,15 @@ public class PrintStreamWithHistory
     //~ Instance/static variables .............................................
 
     private StringWriter  history = new StringWriter();
+    private final String LINE_SEPARATOR = java.security.AccessController
+        .doPrivileged(new java.security.PrivilegedAction<String>()
+        {
+            public String run()
+            {
+                return System.getProperty("line.separator");
+            }
+        });
+    private final byte[] LINE_SEPARATOR_BYTES = LINE_SEPARATOR.getBytes();
 
 
     //~ Constructors ..........................................................
@@ -346,417 +355,55 @@ public class PrintStreamWithHistory
         synchronized (history)
         {
             super.write(buf, off, len);
-            history.write(new String(buf, off, len));
+            int startPos = off;
+            int end = off + len;
+            for (int i = off; i < end; i++)
+            {
+                if (matches(buf, i, end, LINE_SEPARATOR_BYTES))
+                {
+                    if (i > startPos)
+                    {
+                        history.write(new String(buf, startPos, i - startPos));
+                    }
+                    startPos = i + LINE_SEPARATOR_BYTES.length;
+                    i = startPos - 1;
+                    history.write('\n');
+                }
+            }
+            if (startPos < end)
+            {
+                history.write(new String(buf, startPos, end - startPos));
+            }
         }
     }
 
 
     // ----------------------------------------------------------
     /**
-     * Need this private helper function because the PrintStream class
-     * makes its helper private, so we can't override it.  Instead, we
-     * have to redefine our own, and then re-implement all the print
-     * operations on top of it all over again! Argh!
-     *
-     * @param s The string to write
+     * Returns true if the subsequence of elements in buf beginning at
+     * index start matches the entire target array.  If target has length
+     * len, then this method compares the subranges buf[start..start+len-1]
+     * and target[0..len-1], returning true iff they contain the same bytes.
+     * If either range extends off the corresponding array, the result is
+     * false.
+     * @param buf The (possibly larger) array to look in.
+     * @param start The starting index in buf to compare against.
+     * @param target The target sequence of bytes to look for.
      */
-    private void write(char buf[])
+    private boolean matches(byte buf[], int start, int end, byte target[])
     {
-        synchronized (history)
+        int len = target.length;
+        if (start + len > end)
         {
-            super.print(buf);
-            history.write(buf, 0, buf.length);
+            return false;
         }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Send all strings through the superclass print method.
-     *
-     * @param s The string to write
-     */
-    private void write(String s)
-    {
-        super.print(s);
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Send all newlines through the superclass println method.
-     */
-    private void newLine()
-    {
-        super.println();
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a boolean value.  The string produced by <code>{@link
-     * java.lang.String#valueOf(boolean)}</code> is translated into bytes
-     * according to the platform's default character encoding, and these bytes
-     * are written in exactly the manner of the
-     * <code>{@link #write(int)}</code> method.
-     *
-     * @param      b   The <code>boolean</code> to be printed
-     */
-    public void print(boolean b)
-    {
-        write(b ? "true" : "false");
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a character.  The character is translated into one or more bytes
-     * according to the platform's default character encoding, and these bytes
-     * are written in exactly the manner of the
-     * <code>{@link #write(int)}</code> method.
-     *
-     * @param      c   The <code>char</code> to be printed
-     */
-    public void print(char c)
-    {
-        write(String.valueOf(c));
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print an integer.  The string produced by <code>{@link
-     * java.lang.String#valueOf(int)}</code> is translated into bytes
-     * according to the platform's default character encoding, and these bytes
-     * are written in exactly the manner of the
-     * <code>{@link #write(int)}</code> method.
-     *
-     * @param      i   The <code>int</code> to be printed
-     * @see        java.lang.Integer#toString(int)
-     */
-    public void print(int i)
-    {
-        print(String.valueOf(i));
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a long integer.  The string produced by <code>{@link
-     * java.lang.String#valueOf(long)}</code> is translated into bytes
-     * according to the platform's default character encoding, and these bytes
-     * are written in exactly the manner of the
-     * <code>{@link #write(int)}</code> method.
-     *
-     * @param      l   The <code>long</code> to be printed
-     * @see        java.lang.Long#toString(long)
-     */
-    public void print(long l)
-    {
-        write(String.valueOf(l));
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a floating-point number.  The string produced by <code>{@link
-     * java.lang.String#valueOf(float)}</code> is translated into bytes
-     * according to the platform's default character encoding, and these bytes
-     * are written in exactly the manner of the
-     * <code>{@link #write(int)}</code> method.
-     *
-     * @param      f   The <code>float</code> to be printed
-     * @see        java.lang.Float#toString(float)
-     */
-    public void print(float f)
-    {
-        write(String.valueOf(f));
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a double-precision floating-point number.  The string produced by
-     * <code>{@link java.lang.String#valueOf(double)}</code> is translated into
-     * bytes according to the platform's default character encoding, and these
-     * bytes are written in exactly the manner of the <code>{@link
-     * #write(int)}</code> method.
-     *
-     * @param      d   The <code>double</code> to be printed
-     * @see        java.lang.Double#toString(double)
-     */
-    public void print(double d)
-    {
-        write(String.valueOf(d));
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print an array of characters.  The characters are converted into bytes
-     * according to the platform's default character encoding, and these bytes
-     * are written in exactly the manner of the
-     * <code>{@link #write(int)}</code> method.
-     *
-     * @param      s   The array of chars to be printed
-     *
-     * @throws  NullPointerException  If <code>s</code> is <code>null</code>
-     */
-    public void print(char s[])
-    {
-        write(s);
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a string.  If the argument is <code>null</code> then the string
-     * <code>"null"</code> is printed.  Otherwise, the string's characters are
-     * converted into bytes according to the platform's default character
-     * encoding, and these bytes are written in exactly the manner of the
-     * <code>{@link #write(int)}</code> method.
-     *
-     * @param      s   The <code>String</code> to be printed
-     */
-    public void print(String s)
-    {
-        if (s == null) {
-            s = "null";
-        }
-        write(s);
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print an object.  The string produced by the <code>{@link
-     * java.lang.String#valueOf(Object)}</code> method is translated into bytes
-     * according to the platform's default character encoding, and these bytes
-     * are written in exactly the manner of the
-     * <code>{@link #write(int)}</code> method.
-     *
-     * @param      obj   The <code>Object</code> to be printed
-     * @see        java.lang.Object#toString()
-     */
-    public void print(Object obj)
-    {
-        write(String.valueOf(obj));
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Terminate the current line by writing the line separator string.  The
-     * line separator string is defined by the system property
-     * <code>line.separator</code>, and is not necessarily a single newline
-     * character (<code>'\n'</code>).
-     */
-    public void println()
-    {
-        newLine();
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a boolean and then terminate the line.  This method behaves as
-     * though it invokes <code>{@link #print(boolean)}</code> and then
-     * <code>{@link #println()}</code>.
-     *
-     * @param x  The <code>boolean</code> to be printed
-     */
-    public void println(boolean x)
-    {
-        synchronized (history)
+        for (int i = 0; i < len; i++)
         {
-            print(x);
-            newLine();
+            if (buf[start + i] != target[i])
+            {
+                return false;
+            }
         }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a character and then terminate the line.  This method behaves as
-     * though it invokes <code>{@link #print(char)}</code> and then
-     * <code>{@link #println()}</code>.
-     *
-     * @param x  The <code>char</code> to be printed.
-     */
-    public void println(char x)
-    {
-        synchronized (history)
-        {
-            print(x);
-            newLine();
-        }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print an integer and then terminate the line.  This method behaves as
-     * though it invokes <code>{@link #print(int)}</code> and then
-     * <code>{@link #println()}</code>.
-     *
-     * @param x  The <code>int</code> to be printed.
-     */
-    public void println(int x)
-    {
-        synchronized (history)
-        {
-            print(x);
-            newLine();
-        }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a long and then terminate the line.  This method behaves as
-     * though it invokes <code>{@link #print(long)}</code> and then
-     * <code>{@link #println()}</code>.
-     *
-     * @param x  a The <code>long</code> to be printed.
-     */
-    public void println(long x)
-    {
-        synchronized (history)
-        {
-            print(x);
-            newLine();
-        }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a float and then terminate the line.  This method behaves as
-     * though it invokes <code>{@link #print(float)}</code> and then
-     * <code>{@link #println()}</code>.
-     *
-     * @param x  The <code>float</code> to be printed.
-     */
-    public void println(float x)
-    {
-        synchronized (history)
-        {
-            print(x);
-            newLine();
-        }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a double and then terminate the line.  This method behaves as
-     * though it invokes <code>{@link #print(double)}</code> and then
-     * <code>{@link #println()}</code>.
-     *
-     * @param x  The <code>double</code> to be printed.
-     */
-    public void println(double x)
-    {
-        synchronized (history)
-        {
-            print(x);
-            newLine();
-        }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print an array of characters and then terminate the line.  This method
-     * behaves as though it invokes <code>{@link #print(char[])}</code> and
-     * then <code>{@link #println()}</code>.
-     *
-     * @param x  an array of chars to print.
-     */
-    public void println(char x[])
-    {
-        synchronized (history)
-        {
-            print(x);
-            newLine();
-        }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print a String and then terminate the line.  This method behaves as
-     * though it invokes <code>{@link #print(String)}</code> and then
-     * <code>{@link #println()}</code>.
-     *
-     * @param x  The <code>String</code> to be printed.
-     */
-    public void println(String x)
-    {
-        synchronized (history)
-        {
-            print(x);
-            newLine();
-        }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Print an Object and then terminate the line.  This method behaves as
-     * though it invokes <code>{@link #print(Object)}</code> and then
-     * <code>{@link #println()}</code>.
-     *
-     * @param x  The <code>Object</code> to be printed.
-     */
-    public void println(Object x)
-    {
-        synchronized (history)
-        {
-            print(x);
-            newLine();
-        }
-    }
-
-
-    // ----------------------------------------------------------
-    /**
-     * Appends a subsequence of the specified character sequence to this output
-     * stream.
-     *
-     * <p> An invocation of this method of the form <tt>out.append(csq, start,
-     * end)</tt> when <tt>csq</tt> is not <tt>null</tt>, behaves in
-     * exactly the same way as the invocation
-     *
-     * <pre>
-     *     out.print(csq.subSequence(start, end).toString()) </pre>
-     *
-     * @param  csq
-     *         The character sequence from which a subsequence will be
-     *         appended.  If <tt>csq</tt> is <tt>null</tt>, then characters
-     *         will be appended as if <tt>csq</tt> contained the four
-     *         characters <tt>"null"</tt>.
-     *
-     * @param  start
-     *         The index of the first character in the subsequence
-     *
-     * @param  end
-     *         The index of the character following the last character in the
-     *         subsequence
-     *
-     * @return  This character stream
-     *
-     * @throws  IndexOutOfBoundsException
-     *          If <tt>start</tt> or <tt>end</tt> are negative, <tt>start</tt>
-     *          is greater than <tt>end</tt>, or <tt>end</tt> is greater than
-     *          <tt>csq.length()</tt>
-     *
-     * @since  1.5
-     */
-    public PrintStream append(CharSequence csq, int start, int end)
-    {
-        CharSequence cs = (csq == null ? "null" : csq);
-        write(cs.subSequence(start, end).toString());
-        return this;
+        return true;
     }
 }
